7607f60c52131716f155782506e040b61fd2ce3e
v-hill <67915924+v-hill@users.noreply.github.com>
2022-07-13 22:07:08 +0100

    coverge test

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -1,5 +1,7 @@
 """Test downloader.py module."""
 
+import pytest
+
 from src.osmsqlite.downloader import GeofabrikDownloader
 
 
@@ -8,3 +10,13 @@ def test_headers():
     header = downloader._fake_requests_headers()
     assert isinstance(header, dict)
     assert len(header["User-Agent"]) > 0
+
+
+@pytest.mark.xfail
+def test_divide_by_zero():
+    assert 1 / 0 == 1
+
+
+def test_print(capture_stdout):
+    print("hello")
+    assert capture_stdout["stdout"] == "hello\n"
